6d8f8a8bb536093694a9dee96949b9ac99bbd388
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-14 19:15:10 +1000

    Refactor Union.lean

--- ErdosStoneSimonovits/Data/Finset/Union.lean
+++ ErdosStoneSimonovits/Data/Finset/Union.lean
@@ -2,20 +2,20 @@ import Mathlib
 
 namespace Finset
 
-variable {α β : Type*} {s : Finset α}
+variable {α β : Type*}
 
 section DisjiUnion
 
 variable {s : Finset α} {t : Finset β} {f : α → β}
 
-lemma pairwiseDisjoint_filter {f : α → Finset β} (h : (↑s : Set α).PairwiseDisjoint f)
+private lemma pairwiseDisjoint_filter {f : α → Finset β} (h : (↑s : Set α).PairwiseDisjoint f)
     (p : β → Prop) [DecidablePred p] : (s : Set α).PairwiseDisjoint fun a ↦ (f a).filter p :=
   fun _ h₁ _ h₂ hne ↦ Finset.disjoint_filter_filter (h h₁ h₂ hne)
 
 theorem filter_disjiUnion (s : Finset α) (f : α → Finset β) (h) (p : β → Prop) [DecidablePred p] :
     (s.disjiUnion f h).filter p
       = s.disjiUnion (fun a ↦ (f a).filter p) (pairwiseDisjoint_filter h p) := by
-  ext b; simp [mem_filter, mem_disjiUnion, ← exists_and_right, and_assoc]
+  ext; simp [← exists_and_right, and_assoc]
 
 end DisjiUnion
 
