e34932e503b1b86e32ffc895574179f354611e22
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-19 20:40:09 +1000

    Refactor `Colorable.map`

--- ErdosStoneSimonovits/Combinatorics/SimpleGraph/Coloring.lean
+++ ErdosStoneSimonovits/Combinatorics/SimpleGraph/Coloring.lean
@@ -22,24 +22,11 @@ theorem free_of_colorable {α : Type*} {A : SimpleGraph α}
 graph. -/
 theorem Colorable.map {β : Type*} (f : V ↪ β) [NeZero n] (hc : G.Colorable n) :
     (G.map f).Colorable n := by
-  letI := Classical.propDecidable
-  use fun b ↦ if h : ∃ v, f v = b then hc.some h.choose else default
-  intro b₁ b₂ hadj'
-  obtain ⟨v₁, v₂, hadj, hv₁, hv₂⟩ := by
-    rwa [map_adj] at hadj'
-  have hb₁ : ∃ v, f v = b₁ := ⟨v₁, hv₁⟩
-  have hb₁_choose : hb₁.choose = v₁ := by
-    apply f.injective
-    rw [hv₁]
-    exact hb₁.choose_spec
-  have hb₂ : ∃ v, f v = b₂ := ⟨v₂, hv₂⟩
-  have hb₂_choose : hb₂.choose = v₂ := by
-    apply f.injective
-    rw [hv₂]
-    exact hb₂.choose_spec
-  have hne := hc.some.valid hadj
-  rw [← hb₁_choose, ← hb₂_choose] at hne
-  simpa [hb₁, hb₂, hb₁_choose, hb₂_choose] using hc.some.valid hadj
+  obtain ⟨C⟩ := hc
+  use extend f C (const β default)
+  intro a b ⟨_, _, hadj, ha, hb⟩
+  rw [← ha, Injective.extend_apply f.injective, ← hb, Injective.extend_apply f.injective]
+  exact C.valid hadj
 
 /-- If the chromatic number of `G` is `n + 1`, then `G` is colorable in no fewer than `n + 1`
 colors. -/
