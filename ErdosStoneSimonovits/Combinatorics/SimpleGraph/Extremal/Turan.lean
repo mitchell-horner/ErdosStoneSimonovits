b5fb1111f3286c9f3bf8fd8efb2c7dde22bca864
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-08-23 09:52:28 +1000

    Refactor Turan.lean

--- ErdosStoneSimonovits/Combinatorics/SimpleGraph/Extremal/Turan.lean
+++ ErdosStoneSimonovits/Combinatorics/SimpleGraph/Extremal/Turan.lean
@@ -1,4 +1,5 @@
 import Mathlib
+import ErdosStoneSimonovits.Combinatorics.SimpleGraph.Clique
 import ErdosStoneSimonovits.Combinatorics.SimpleGraph.Extremal.Basic
 import ErdosStoneSimonovits.Combinatorics.SimpleGraph.Finite
 
@@ -9,31 +10,20 @@ open Finset Fintype
 lemma turanGraph_adj {n r v w} :
   (turanGraph n r).Adj v w ↔ v % r ≠ w % r := by rfl
 
-variable {V : Type*} {G : SimpleGraph V} {n : ℕ} {α : Type*} [Fintype α]
-
-/-- A simple graph does not contain `⊤ : SimpleGraph α` iff it has no `card α`-cliques. -/
-theorem top_free_iff_cliqueFree :
-    (⊤ : SimpleGraph α).Free G ↔ G.CliqueFree (card α) := by
-  rw [← not_iff_not, not_free, cliqueFree_iff, not_isEmpty_iff,
-    isContained_congr (Iso.completeGraph (Fintype.equivFin α)) Iso.refl]
-  refine ⟨fun ⟨f⟩ ↦ ⟨f.toEmbedding, ⟨?_, f.toHom.map_adj⟩⟩, fun ⟨f⟩ ↦ ⟨f.toCopy⟩⟩
-  simpa [← f.injective.ne_iff] using G.ne_of_adj
-
-variable [Fintype V] [DecidableRel G.Adj] [Nontrivial α]
+variable {V : Type*} [Fintype V] {G : SimpleGraph V} [DecidableRel G.Adj]
+  {n : ℕ} {α : Type*} [Fintype α] [Nontrivial α]
 
 lemma isExtremal_top_free_iff_isTuranMaximal :
     G.IsExtremal (⊤ : SimpleGraph α).Free ↔ G.IsTuranMaximal (card α - 1) := by
   simp_rw [IsTuranMaximal, IsExtremal,
-    Nat.sub_one_add_one Fintype.card_ne_zero, top_free_iff_cliqueFree]
+    Nat.sub_one_add_one Fintype.card_ne_zero, cliqueFree_iff_top_free]
 
 lemma isExtremal_top_free_turanGraph :
     (turanGraph n (card α - 1)).IsExtremal (⊤ : SimpleGraph α).Free := by
   rw [isExtremal_top_free_iff_isTuranMaximal]
   exact isTuranMaximal_turanGraph (Nat.sub_pos_iff_lt.mpr Fintype.one_lt_card)
 
-/-- The extremal numbers of `⊤` are equal to the number of edges in `turanGraph`.
-
-This is a corollary of **Turán's theorem**. See `SimpleGraph.isTuranMaximal_turanGraph`. -/
+/-- The extremal numbers of `⊤` are equal to the number of edges in `turanGraph`. -/
 theorem extremalNumber_top :
     extremalNumber n (⊤ : SimpleGraph α) = #(turanGraph n (card α - 1)).edgeFinset := by
   conv =>
@@ -43,9 +33,9 @@ theorem extremalNumber_top :
 
 /-- The `turanGraph` is, up to isomorphism, the unique extremal graph forbidding `⊤`.
 
-This is a corollary of **Turán's theorem**.
+This is **Turán's theorem** restated in terms of the extremal numbers of `⊤`.
 See `SimpleGraph.isTuranMaximal_iff_nonempty_iso_turanGraph`. -/
-theorem card_edgeFinset_eq_extremalNumber_top_iff_iso_turanGraph :
+theorem card_edgeFinset_eq_extremalNumber_top_iff_nonempty_iso_turanGraph :
     (⊤ : SimpleGraph α).Free G ∧ #G.edgeFinset = extremalNumber (card V) (⊤ : SimpleGraph α)
       ↔ Nonempty (G ≃g turanGraph (card V) (card α - 1)) := by
   rw [← isTuranMaximal_iff_nonempty_iso_turanGraph (Nat.sub_pos_iff_lt.mpr one_lt_card),
