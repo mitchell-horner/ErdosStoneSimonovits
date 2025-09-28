17665314d097de69840444924608e7a636b9f2a0
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-09-28 21:42:53 +1000

    Remove `Fintype.`

--- ErdosStoneSimonovits/Combinatorics/SimpleGraph/Extremal/Turan.lean
+++ ErdosStoneSimonovits/Combinatorics/SimpleGraph/Extremal/Turan.lean
@@ -16,12 +16,12 @@ variable {V : Type*} [Fintype V] {G : SimpleGraph V} [DecidableRel G.Adj]
 lemma isExtremal_top_free_iff_isTuranMaximal :
     G.IsExtremal (⊤ : SimpleGraph α).Free ↔ G.IsTuranMaximal (card α - 1) := by
   simp_rw [IsTuranMaximal, IsExtremal,
-    Nat.sub_one_add_one Fintype.card_ne_zero, cliqueFree_iff_top_free]
+    Nat.sub_one_add_one card_ne_zero, cliqueFree_iff_top_free]
 
 lemma isExtremal_top_free_turanGraph :
     (turanGraph n (card α - 1)).IsExtremal (⊤ : SimpleGraph α).Free := by
   rw [isExtremal_top_free_iff_isTuranMaximal]
-  exact isTuranMaximal_turanGraph (Nat.sub_pos_iff_lt.mpr Fintype.one_lt_card)
+  exact isTuranMaximal_turanGraph (Nat.sub_pos_iff_lt.mpr one_lt_card)
 
 /-- The extremal numbers of `⊤` are equal to the number of edges in `turanGraph`. -/
 theorem extremalNumber_top :
