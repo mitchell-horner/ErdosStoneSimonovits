b87d10ea9656f6943248390cb86854192de4fe13
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-11-26 22:59:17 +1100

    Replace `range_add_nat_eq_image_nat_Ici` with `range_add_eq_image_Ici`

---
+++ ErdosStoneSimonovits/Algebra/Order/Monoid/Canonical/Basic.lean
@@ -0,0 +1,12 @@
+import Mathlib
+
+namespace Set
+
+variable {α : Type*} [CanonicallyOrderedAddCommMonoid α] [Sub α] [OrderedSub α]
+
+theorem range_add_eq_image_Ici {β : Type*} {f : α → β} {k : α} :
+    range (fun x ↦ f (x + k)) = f '' Ici k :=
+  Set.ext fun x ↦ ⟨fun ⟨y, hfy⟩ ↦ ⟨y + k, self_le_add_left k y, hfy⟩,
+    fun ⟨y, hy, hfy⟩ ↦ ⟨y - k, by simp [tsub_add_cancel_of_le hy, hfy]⟩⟩
+
+end Set
--- ErdosStoneSimonovits/Topology/Instances/NNReal/Lemmas.lean
+++ ErdosStoneSimonovits/Topology/Instances/NNReal/Lemmas.lean
@@ -1,5 +1,5 @@
 import Mathlib
-import ErdosStoneSimonovits.Data.Set.Image
+import ErdosStoneSimonovits.Algebra.Order.Monoid.Canonical.Basic
 import ErdosStoneSimonovits.Order.Monotone.Basic
 
 open Filter Set Topology
@@ -31,22 +31,22 @@ limit `sSup (f '' Ici k)`. -/
 theorem _root_.Real.tendsto_csSup_of_bddAbove_monotoneOn_Ici {f : ℕ → ℝ} {k : ℕ}
     (h_bdd : BddAbove (f '' Ici k)) (h_mon : MonotoneOn f (Ici k)) :
     Tendsto f atTop (𝓝 (sSup (f '' Ici k))) := by
-  rw [← range_add_nat_eq_image_nat_Ici] at h_bdd
+  rw [← range_add_eq_image_Ici] at h_bdd
   rw [← monotone_add_nat_iff_monotoneOn_nat_Ici] at h_mon
   have h := Real.tendsto_ciSup_of_bddAbove_monotone h_bdd h_mon
   rwa [tendsto_add_atTop_iff_nat k, ← sSup_range,
-    range_add_nat_eq_image_nat_Ici, image] at h
+    range_add_eq_image_Ici, image] at h
 
 /-- An antitone, bounded below sequence `f : ℕ → ℝ` on `Ici k` has the finite
 limit `sInf (f '' Ici k)`. -/
 theorem _root_.Real.tendsto_csInf_of_bddBelow_antitoneOn_Ici {f : ℕ → ℝ} {k : ℕ}
     (h_bdd : BddBelow (f '' Ici k)) (h_ant : AntitoneOn f (Ici k)) :
     Tendsto f atTop (𝓝 (sInf (f '' Ici k))) := by
-  rw [← range_add_nat_eq_image_nat_Ici] at h_bdd
+  rw [← range_add_eq_image_Ici] at h_bdd
   rw [← antitone_add_nat_iff_antitoneOn_nat_Ici] at h_ant
   have h := Real.tendsto_ciInf_of_bddBelow_antitone h_bdd h_ant
   rwa [tendsto_add_atTop_iff_nat k, ← sInf_range,
-    range_add_nat_eq_image_nat_Ici, image] at h
+    range_add_eq_image_Ici, image] at h
 
 /-- The limit of a monotone, bounded above sequence `f : ℕ → ℝ` is a least upper bound
 of the sequence. -/
