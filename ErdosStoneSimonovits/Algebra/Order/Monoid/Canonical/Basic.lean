9b5ce6120f724be93d232f46e3c5076715971e60
mitchell-horner <29882987+mitchell-horner@users.noreply.github.com>
2025-11-27 00:32:14 +1100

    Refactor `range_add_eq_image_Ici`

--- ErdosStoneSimonovits/Algebra/Order/Monoid/Canonical/Basic.lean
+++ ErdosStoneSimonovits/Algebra/Order/Monoid/Canonical/Basic.lean
@@ -3,9 +3,9 @@ import Mathlib
 namespace Set
 
 variable {α : Type*} [CanonicallyOrderedAddCommMonoid α] [Sub α] [OrderedSub α]
+  {β : Type*} {f : α → β} {k : α}
 
-theorem range_add_eq_image_Ici {β : Type*} {f : α → β} {k : α} :
-    range (fun x ↦ f (x + k)) = f '' Ici k :=
+theorem range_add_eq_image_Ici : range (fun x ↦ f (x + k)) = f '' Ici k :=
   Set.ext fun x ↦ ⟨fun ⟨y, hfy⟩ ↦ ⟨y + k, self_le_add_left k y, hfy⟩,
     fun ⟨y, hy, hfy⟩ ↦ ⟨y - k, by simp [tsub_add_cancel_of_le hy, hfy]⟩⟩
 
